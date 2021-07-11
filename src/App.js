86ace64c9c0ba65c38fbb21f33db098bba4103bc
Natalia Ignateva <ignatyevanat@gmail.com>
2021-07-11 22:21:30 +0200

    perform HTTP request to GitHub, create Card and CardList components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Form from './components/Form';
+import CardList from './components/CardList';
 
 function App() {
+  const [cards, setCards] = useState([]);
+  const addNewCard = (card) => {
+    setCards([...cards, card]);
+  };
+
   return (
     <div className="App">
       <h1 className="Title">Search a GitHub User</h1>
-      <Form />
+      <Form onSubmit={addNewCard} />
+      <CardList cards={cards} />
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,22 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
-const Form = () => {
-  const [userName, setUsername] = useState('');
+const Form = (props) => {
+  const [username, setUsername] = useState('');
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    alert(userName);
+
+    axios.get(`https://api.github.com/users/${username}`).then((resp) => {
+      props.onSubmit(resp.data);
+      setUsername('');
+    });
   };
+
   return (
     <form className="Search" onSubmit={handleSubmit}>
       <input
-        value={userName}
+        value={username}
         onChange={(event) => setUsername(event.target.value)}
         type="text"
         placeholder="GitHub username"
