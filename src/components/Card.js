552528053f8b9a914cd9c7ccf66de22f0477fd7e
Natalia Ignateva <ignatyevanat@gmail.com>
2021-07-12 10:21:29 +0200

    fix website button parameter

--- src/components/Card.js
+++ src/components/Card.js
@@ -38,7 +38,7 @@ const Card = (props) => {
               Profile
             </button>
           </a>
-          <a href={props.html_url}>
+          <a href={props.blog || ''}>
             <button className="mr-2 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm leading-5 font-medium rounded-md text-gray-700 bg-white hover:text-gray-500 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 active:bg-gray-50 active:text-gray-800">
               Website
             </button>
